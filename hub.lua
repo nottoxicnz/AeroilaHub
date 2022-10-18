97b08e0c292d61f37e5d579fee4711dccff2d5fb
nottoxicnz <106415599+nottoxicnz@users.noreply.github.com>
2022-10-18 15:14:14 +1300

    Update and rename SCRIPT HUB to hub.lua

--- hub.lua
+++ hub.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 10232731562 then 
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/nottoxicnz/AeroilaHub/main/src.lua')))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/nottoxicnz/AeroilaHub/main/script.lua')))()
 elseif game.PlaceId == 9144187696 then 
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/nottoxicnz/AeroilaHub/main/A%20Piece')))()
 elseif game.PlaceId == 9744597824 then
